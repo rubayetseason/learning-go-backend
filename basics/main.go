08b8f7bdfc602a48ee17bd52ec4ac4e9f0b40b89
rubayetseason <rubayetislamseason5@gmail.com>
2025-10-09 19:21:55 +0600

    first commit

---
+++ basics/main.go
@@ -0,0 +1,9 @@
+package main // must needed package for each files
+
+import "fmt"
+//fmt --> format; has lots of functions inside
+
+func main() {
+	// this main function is the package imported
+	fmt.Println("Hello, World!")
+}

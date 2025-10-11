84d9280cb26f8cc0702418ba53398232203336d9
rubayetseason <rubayetislamseason5@gmail.com>
2025-10-11 14:12:31 +0600

    higher order function

---
+++ basics/functions/higher-order.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// higher order function that takes function as params
+func processOperation(a int, b int, op func(p int, q int)) {
+	op(a, b)
+}
+
+// higher order function that returns function
+func call() func(x int, y int) {
+	return add
+}
+
+func add(x int, y int) {
+	fmt.Println("The sum is ~ ", x+y)
+}
+
+func main() {
+	processOperation(10, 20, add)
+
+	sum := call()
+	sum(10, 20)
+}
+
+// higher order function is a function which accepts a function as an argument or returns a function

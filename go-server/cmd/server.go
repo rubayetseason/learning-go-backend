1a5def2eb7959a5ac577039bee4d8463ed0d755d
rubayetseason <rubayetislamseason5@gmail.com>
2025-10-27 16:16:38 +0600

    godotenv package utilized

--- go-server/cmd/server.go
+++ go-server/cmd/server.go
@@ -3,11 +3,17 @@ package cmd
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
+	"ecommerce/config"
 	"ecommerce/middleware"
 )
 
 func Server() {
+
+	cnf := config.GetConfig()
+
 	manager := middleware.NewManager()
 	manager.Use(middleware.PreflightHandler,
 		middleware.CorsHandler,
@@ -19,11 +25,13 @@ func Server() {
 
 	initRoutes(mux, manager)
 
-	fmt.Println("🚀 Golang server is running on: 5000")
+	portAddress := ":" + strconv.Itoa(cnf.HttpPort)
+	fmt.Println("🚀 Golang server is running on port", portAddress)
 
-	err := http.ListenAndServe(":5000", wrappedMux) // if no error then will get nill else will get the error text
+	err := http.ListenAndServe(portAddress, wrappedMux) // if no error then will get nill else will get the error text
 
 	if err != nil {
 		fmt.Println("Error starting the server", err)
+		os.Exit(1)
 	}
 }

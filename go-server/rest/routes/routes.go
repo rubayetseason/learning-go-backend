0eb22029af30cd2b3a613e382db48a6dbd4b6573
rubayetseason <rubayetislamseason5@gmail.com>
2025-11-08 16:02:41 +0600

    authentication middleware

--- go-server/rest/routes/routes.go
+++ go-server/rest/routes/routes.go
@@ -9,11 +9,11 @@ import (
 )
 
 func InitRoutes(mux *http.ServeMux, manager *middleware.Manager) {
-	mux.Handle("POST /create-product", manager.With(http.HandlerFunc(products.CreateProductHandler)))
+	mux.Handle("POST /create-product", manager.With(http.HandlerFunc(products.CreateProductHandler), middleware.AuthenticateJwt))
 	mux.Handle("GET /products", manager.With(http.HandlerFunc(products.GetProductsHandler), middleware.EndPrint))
-	mux.Handle("GET /products/{productId}", manager.With(http.HandlerFunc(products.GetProductByIdHandler)))
-	mux.Handle("PUT /products/{productId}", manager.With(http.HandlerFunc(products.UpdateProductHandler)))
-	mux.Handle("DELETE /products/{productId}", manager.With(http.HandlerFunc(products.DeleteProductHandler)))
+	mux.Handle("GET /products/{productId}", manager.With(http.HandlerFunc(products.GetProductByIdHandler), middleware.AuthenticateJwt))
+	mux.Handle("PUT /products/{productId}", manager.With(http.HandlerFunc(products.UpdateProductHandler), middleware.AuthenticateJwt))
+	mux.Handle("DELETE /products/{productId}", manager.With(http.HandlerFunc(products.DeleteProductHandler), middleware.AuthenticateJwt))
 
 	mux.Handle("POST /create-user", manager.With(http.HandlerFunc(users.CreateUserHandler)))
 	mux.Handle("POST /users/login", manager.With(http.HandlerFunc(users.LoginUserHandler)))
